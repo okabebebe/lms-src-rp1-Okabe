6b51dbdc4e46a597102353f9432ed3703f775286
h-okabe <h-okabe@PLPC34>
2026-01-16 14:54:29 +0900

    add:岡部 11行-14行目、フォーマットパターン追加＋今日の日付取得機能追加

--- trial_source/src/main/java/jp/co/sss/lms/util/SimpleDateFormat.java
+++ trial_source/src/main/java/jp/co/sss/lms/util/SimpleDateFormat.java
@@ -1,5 +1,18 @@
 package jp.co.sss.lms.util;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class SimpleDateFormat {
+	
+	public DateTimeFormatter date =DateTimeFormatter.ofPattern("yyyy/MM/dd");
+	
+	public LocalDateTime today() {
+		return LocalDateTime.now();
+		
+	}
 
 }
